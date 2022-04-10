8ef457e77b8ddd66921303aca0dc82f8a81f99f7
darinextremeconsulting <62944904+darinextremeconsulting@users.noreply.github.com>
2022-04-10 18:50:11 +0300

    feat: add routing

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import { vuetify } from "./plugins/vuetify";
+import router from "./router";
+import Datepicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
+import * as firebase from "firebase/app"
+import { initializeApp } from "firebase/app"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDgrZfPkhTmcNbzwhXTZwJaV3iFRQ_gVRE",
+    authDomain: "nasa-api-fd268.firebaseapp.com",
+    projectId: "nasa-api-fd268",
+    storageBucket: "nasa-api-fd268.appspot.com",
+    messagingSenderId: "501108916287",
+    appId: "1:501108916287:web:341572edc61f21f580388a"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const app = createApp(App).use(router).use(vuetify)
+
+app.component("Datepicker", Datepicker); // global registration - can be used anywhere
+
+app.mount("#app");
+
---
+++ src/router/index.ts
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/earth',
+    name: 'Earth',
+    component: () => import('../views/Earth.vue')
+  },
+  {
+    path: '/epic',
+    name: 'Epic',
+    component: () => import('../views/Epic.vue')
+  },
+  {
+    name: 'Register',
+    path: "/register",
+    component: () => import("../views/Register.vue"),
+  },
+  // {
+  //   path: "/sign-in",
+  //   component: () => import("../views/SignIn.vue"),
+  // },
+  // {
+  //   path: '/:catchAll(.*)',
+  //   name: 'PageNotFound',
+  //   component: () => import('../views/PageNotFound.vue')
+  // }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+const menu = routes
+
+export {
+  router as default,
+  menu
+}
