dfcebd8a53414c2169700a7cb1ef49ab6fdbf5c3
darinextremeconsulting <62944904+darinextremeconsulting@users.noreply.github.com>
2022-04-10 18:51:50 +0300

    feat: add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+
+
+export const nasaApiKey = 'e5CcQxToNUuSnlPM4HI7rRTdCeotXOiuGdMAfL0y'
+export const baseUrlApodApi = 'https://api.nasa.gov/planetary/apod?api_key='
+export const baseUrlEpicAvailable = 'https://api.nasa.gov/EPIC/api/natural/available'
+export const baseUrlEarthApi = 'https://api.nasa.gov/planetary/earth/assets?'
+export const epicImgeBaseUrl = 'https://epic.gsfc.nasa.gov/archive/natural/'
+export const long = '27.914734'
+export const lat = '43.214050'
